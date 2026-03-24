f02b9406bac94ac2d437a2ad4eadc3f421826c93
Hasan Tepe <hasantepe@Hasan-MacBook-Pro.local>
2026-03-24 22:22:44 +0300

    Branding: Integrated custom favicon.png

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
   title: 'Shamrock | Menu',
   description: 'Shamrock Restaurant Menu',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
